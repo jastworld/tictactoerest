eaea9597678dc206e8fcfd0988191090226b4df9
Ubuntu <ubuntu@nodeinstance.cloud.compas.cs.stonybrook.edu>
2018-02-15 15:23:55 +0000

    current version

--- api/routes/play.js
+++ api/routes/play.js
@@ -35,6 +35,11 @@ var checkwinner = (game)=>{
     return "";
 };
 router.post('/',(req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
     const game = {
         grid: req.body.grid
     }
--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const playRoute = require('./api/routes/play');
-
+app.use(cors({origin: '*'}));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
